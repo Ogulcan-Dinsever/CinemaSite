d042c1833f1a39c0bf0e6061a3bf0f686aea17a4
Oğulcan Dinsever <ogulcandnsvr@gmail.com>
2021-06-27 02:11:19 +0300

    Login ve Register Sayfaları eklendi.

--- CinemaSite.UI/App_Start/RouteConfig.cs
+++ CinemaSite.UI/App_Start/RouteConfig.cs
@@ -14,10 +14,23 @@ namespace CinemaSite.UI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Login",
+                url: "login",
+                defaults: new { controller = "Account", action = "Login" }
+            );
+
+            routes.MapRoute(
+                name: "Register",
+                url: "register",
+                defaults: new { controller = "Account", action = "Register" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
